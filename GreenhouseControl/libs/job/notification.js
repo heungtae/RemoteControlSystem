0938d023dd7811f5ec8fe2b6d89714a7dc7409bc
heungtae <heungtae@gmail.com>
2016-05-28 15:09:34 +0900

    completed notifications

--- GreenhouseControl/libs/job/notification.js
+++ GreenhouseControl/libs/job/notification.js
@@ -1,32 +1,46 @@
 var CronJob = require('cron').CronJob,
+	store = require('../db/notification'),
+	env = require('../db/environment'),
 	ghConfig = require('../../ghConfig'),
-	store = require('../db/scheduleSprinkler'),
-	sprinkler = require('../../routes/sprinkler')
+	sleep = require('../sleep'),
 	log4js = require('log4js'),
 	send = require('../telegram/send');
 
-var	log = log4js.getLogger('libs.job.scheduleSprinkler');
+var	log = log4js.getLogger('libs.job.scheduleShutter');
 log.setLevel(config.loglevel);
 
 var jobCompletedList = [];
 var jobExecuteList = [];
 
+// 1. 실행 시간의 구간안에 포함되는지 확인 한다.
+// 1.1. 실행 시간이 아니라면 실행 중인 항목에 대해서 삭제 한다.
+// 2. 실행 시간의 구간안에 실행 중인지를 확인 한다.
+// 3. 환경값을 읽어와서 필요한 환경 값을 Setting 한다.
+// 4. 실행 가능한지를 확인 한다.
+// 5. 실행한다.
 
-var job = new CronJob('*/10 * * * * *', function() {
+var job = new CronJob('*/10 * * * *', function() {
 	try{	
-				
-		store.get(function(err, docs, confs){
+		var d = new Date();
+		var m = d.getMinutes();
+		//10분에 한번씩 환경데이터를 알림하기 위해서 jobCompletedList를 삭제 하도록 한다.
+		if(m % 10 == 0 ){
+			jobCompletedList = [];
+		}
+		
+		store.get(function(err, docs, conf){
+			//시간, 환경값, 실행 여부를 확인한다. 
 			jobExecuteList = [];
 			
 			for(var key in docs){
 				var doc = docs[key];
-				
 				checkTime(doc, function(doc){
 					checkEnvironment(doc, function(doc){
 						checkJobCompleted(doc, function(doc){
-							if(doc.inTime && doc.isEnvironment && !isJobCompleted ){
+							if(doc.inTime && doc.isEnvironment && !doc.isJobCompleted ){
 								checkPriority(doc, function(){
 									
+									send.message(doc.title + '으로 예약된 창 제어를 시작했습니다. \n ' + doc.alias + '를 ' + stepDesc);
 								});
 							}
 						});
@@ -34,46 +48,8 @@ var job = new CronJob('*/10 * * * * *', function() {
 				});
 			}
 			
-			for(i = 0; i < jobExecuteList.length; i++){
-				var doc = jobExecuteList[i];
-							
-				getConf(doc, confs, function(doc){
-					if(result){
-						var data = {
-								unit: doc.baseconf.unit,
-								settime: doc.period.trim(),
-								exectime: 0,
-								pin: 0,
-								command: 'on'
-						};
-						
-						sprinkler.updateJob(data, function(){
-							log.debug('[CronJob] ' + doc.title +'(' + doc.id + ') starting sprinkler base unit: ' + JSON.stringify(data));	
-							
-						});
-						
-						data = {
-								unit: doc.unit.trim(),
-								settime: doc.period.trim(),
-								exectime: 0,
-								pin: 0,
-								command: 'on'
-						};
-						
-						sprinkler.updateJob(data, function(){
-							log.debug('[CronJob] ' + doc.title +'(' + doc.id + ') starting sprinkler: ' + JSON.stringify(data));	
-							send.message(doc.title + '으로 예약된 자동관수를 시작했습니다. \n ' + doc.alias + '를 ' + doc.period.trim() + '동안 실행합니다.'); 
-							jobCompletedList.push(doc.id);
-						});
-					}
-					
-				});
-				
-			}
-			
 			clearJobCompletedList(docs);
 		});
-		
 	}catch(e){
 		log.error(e);
 	}
@@ -81,27 +57,23 @@ var job = new CronJob('*/10 * * * * *', function() {
 );
 
 var checkTime = function(doc, callback){
-	var now = new Date();
-	var start = new Date();
-	//var end = new Date();
-	
-	start.setHours(doc.start.substring(0, 2), doc.start.substring(3, 5), 00, 00)
-	var end = new Date(start.getTime() + doc.period * 60 * 1000);
-	
-	var yearStart = new Date(now.getFullYear(), 0, 0);
-	var diff = now - yearStart;
-	var oneDay = 1000 * 60 * 60 * 24;
-	var day = Math.floor(diff / oneDay);
-	
-	log.trace('[CronJob] ' + doc.title +'(' + doc.id + ') ' + start + ' ~ ' + end + ' & ' + now);
-	log.trace('[CronJob] ' + doc.title +'(' + doc.id + ') ' + day + ' ' + doc.cycle);
-	
-	if((now.getTime() > start.getTime() && now.getTime() < end.getTime()) && (day % doc.cycle == 0)){
-		doc.inTime = true;
+	if(doc.star != undefined){
+		var now = new Date(),
+		start = new Date(),
+		end = new Date();
+		
+		start.setHours(doc.start.substring(0, 2), doc.start.substring(3, 5), 00, 00);
+		end.setHours(doc.end.substring(0, 2), doc.end.substring(3, 5), 59, 59);
+		
+		log.debug('[checkTime] ' + doc.title +'(' + doc.id + ') ' + ' : ' + start + ' ' + end);
+		if(now.getTime() > start.getTime() && now.getTime() < end.getTime()){
+			doc.inTime = true;
+		}else{
+			doc.inTime = false;
+		}
 	}else{
-		doc.inTime = false;
+		doc.inTime = true;
 	}
-	
 	callback(doc);
 }
 
@@ -154,57 +126,44 @@ var checkEnvironment = function(doc, callback){
 		
 		callback(doc);
 	});
-};
+}
 
 var checkJobCompleted = function(doc, callback){
 	doc.isJobCompleted = jobCompletedList[doc.id] == undefined ? false : true;
 	callback(doc);
 }
-
+		
 var checkPriority = function(newDoc, callback){
-	var unitKey = newDoc.unit;
+	var unitKey = newDoc.side + '-' + newDoc.position;
 	
-	jobExecuteList[unitKey] = newDoc;
+	if(jobExecuteList[unitKey] == undefined){
+		jobExecuteList[unitKey] = newDoc;
+	}else{
+		var doc = jobExecuteList[unitKey];
+		//compare environment value 
+		for(key in doc){
+			if(key.endsWith('Value')){
+				if(newDoc[key] == undefined){
+					isSameKey = false;
+					break;
+				}
+			}
+		}
+		
+		if(isSameKey){
+			for(key in doc){
+				if(key.endsWith('Value')){
+					var operKey = key.replace('Value', 'Oper');
+					if(operKey == '>' && parseInt(doc[key]) < parseInt(newDoc[key])){
+						jobExecuteList[unitKey] = newDoc;
+					}else if(parseInt(doc[key]) > parseInt(newDoc[key])){
+						jobExecuteList[unitKey] = newDoc;	
+					}
+				}
+			}
+		}
+	}
 	
 	callback();
 }
 
-var getConf = function(doc, confs, callback){
-	
-	try{
-		log.trace('[getConf] ' + doc.title +'(' + doc.id + ') found shutter documents: count= ' + confs.length);
-		
-		var conf;
-		for(var i =0; i < confs.length; i ++){
-			val = confs[i];
-			if(val.unit == doc.unit.trim()){
-				doc.conf = val;
-				break;
-			} 
-		}
-		
-		
-		for(var i =0; i < confs.length; i ++){
-			val = confs[i];
-			if(val.baseunit){
-				doc.baseconf = val;
-				break;
-			} 
-		}
-		
-		callback(doc);
-	}catch(e){
-		log.error(e);
-		callback(doc);
-	}
-	
-};
-
-var clearJobCompletedList = function(docs){
-	for(var id in docs){
-		if(jobCompletedList[docs[id].id] != undefined)
-			delete jobCompletedList[key]; 
-	}
-	
-	log.trace('[clearJobCompletedList] Job Completed List :' + JSON.stringify(jobCompletedList));
-}
